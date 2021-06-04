52313744052e5c3e22e7f40b5b94f99670b44c86
Tsuyoshi Yamamoto <v20e3023@oita-u.ac.jp>
2021-06-04 12:23:45 +0000

    Lesson7の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,11 @@ class PostsController < ApplicationController
     
     def index
         @posts = Post.all.order(created_at: 'desc')
+        render 'posts/index' #デフォルトのため省略可能
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+
 end
