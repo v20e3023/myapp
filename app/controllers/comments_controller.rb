c4df285392f8e53bf52ad7e2b54a533d8ca0a81b
Tsuyoshi Yamamoto <v20e3023@oita-u.ac.jp>
2021-07-05 08:46:35 +0000

    Lesson12の課題

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+    
+    def create
+        @post = Post.find(params[:post_id])
+        @post.comments.create(comments_params)
+        redirect_to post_path(@post)
+    end
+    
+    private
+    
+    def comments_params
+        params.require(:comment).permit(:body)
+    end
 end
